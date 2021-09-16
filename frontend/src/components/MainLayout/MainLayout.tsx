d1c79112a7637c43093cd1b9a7cbb6f5d65a9ffb
Pierre Poitier <hanofound@hotmail.com>
2021-09-16 13:08:11 +0200

    # Rework main layout

--- frontend/src/components/MainLayout/MainLayout.tsx
+++ frontend/src/components/MainLayout/MainLayout.tsx
@@ -1,24 +1,18 @@
 import React from "react";
-import {Sidebar} from "../Sidebar/Sidebar";
 import "./MainLayout.scss";
-import {Navbar} from "../Navbar/Navbar";
+import { Navbar } from "../Navbar/Navbar";
+import { Sidebar } from "../Sidebar/Sidebar";
 
 export class MainLayout extends React.PureComponent {
-
-    render() {
-        return (
-            <div className="main-layout">
-                <Sidebar/>
-                <div className="main-layout--container">
-                    <div className="main-layout--navbar-container sticky-lg-top">
-                        <Navbar/>
-                    </div>
-                    <div className="container">
-                        {this.props.children}
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
+  render() {
+    return (
+      <div className="main-layout">
+        <Navbar />
+        <div className="main-layout__body">
+          <Sidebar />
+          <div className="main-layout__content">{this.props.children}</div>
+        </div>
+      </div>
+    );
+  }
 }
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -2,42 +2,57 @@ import React from "react";
 import "./Navbar.scss";
 
 export class Navbar extends React.Component<any, any> {
-
-    render() {
-        return (
-            <nav className="navbar navbar-expand-lg navbar-light" id="navbar">
-                <div className="container-fluid">
-                    <button className="navbar-toggler" type="button"
-                            data-bs-toggle="collapse" data-bs-target="#navbarContent"
-                            aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"/>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <span className="nav-link">Accueil</span>
-                            </li>
-                            <li className="nav-item">
-                                <span className="nav-link">Information</span>
-                            </li>
-                        </ul>
-                        <form className="me-auto d-flex">
-                            <input className="form-control me-2" type="search" placeholder="Sujet..."
-                                   aria-label="Search"/>
-                            <button className="btn btn-outline-success" type="submit">Rechercher</button>
-                        </form>
-                        <ul className="navbar-nav me-4 mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <span className="nav-link">Connexion</span>
-                            </li>
-                            <li className="nav-item">
-                                <span className="nav-link">Inscription</span>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        );
-    }
-
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light" id="navbar">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarContent"
+            aria-controls="navbarContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <img
+            className="navbar__logo"
+            src={process.env.PUBLIC_URL + "/images/logo_cslabs.png"}
+            alt="CSLabs Logo"
+          />
+          <div className="collapse navbar-collapse" id="navbarContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <span className="nav-link">Accueil</span>
+              </li>
+              <li className="nav-item">
+                <span className="nav-link">Information</span>
+              </li>
+            </ul>
+            <form className="me-auto d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Sujet..."
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Rechercher
+              </button>
+            </form>
+            <ul className="navbar-nav me-4 mb-2 mb-lg-0">
+              <li className="nav-item">
+                <span className="nav-link">Connexion</span>
+              </li>
+              <li className="nav-item">
+                <span className="nav-link">Inscription</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
